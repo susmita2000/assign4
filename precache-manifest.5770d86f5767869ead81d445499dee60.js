7d6bc1591b82b62e3f1d74d13c25a1a48cf1080b
Susmita Mazumdar <SUSMITA10@IITKGP.AC.IN>
2019-07-11 16:02:25 +0530

    Updates

---
+++ precache-manifest.5770d86f5767869ead81d445499dee60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d972a97dc8bf8efc1c7877f6bb4db9",
+    "url": "susmita2000.github.io/assign4/index.html"
+  },
+  {
+    "revision": "48883437447d35eb4773",
+    "url": "susmita2000.github.io/assign4/static/css/main.0140acf4.chunk.css"
+  },
+  {
+    "revision": "d2e319b55b78d80959b2",
+    "url": "susmita2000.github.io/assign4/static/js/2.13a055de.chunk.js"
+  },
+  {
+    "revision": "48883437447d35eb4773",
+    "url": "susmita2000.github.io/assign4/static/js/main.b99330ab.chunk.js"
+  },
+  {
+    "revision": "057c29a81908f5ea06da",
+    "url": "susmita2000.github.io/assign4/static/js/runtime~main.d9eb19bb.js"
+  }
+]);
